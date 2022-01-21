58b26ab01054e7cd2013d1653d80571e3e445d33
bruno <brunoparma@hotmail.com>
2022-01-21 11:03:29 -0300

    change spec description

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -19,12 +19,6 @@ describe Payment do
       it 'is valid with valid attributes' do
         expect(subject).to be_valid
       end
-
-      it do
-        is_expected.to validate_numericality_of(:amount)
-          .is_greater_than_or_equal_to(Payment::MIN_AMOUNT)
-          .is_less_than_or_equal_to(Payment::MAX_AMOUNT)
-      end
     end
 
     describe 'same user' do
@@ -33,7 +27,7 @@ describe Payment do
       subject(:create_payment) do
         create :payment, user: user, friend: user
       end
-      it 'raises an error if is already active' do
+      it 'raises an error if is the same user' do
         error_message = /#{I18n.t('api.error.payment_same_account')}/
         expect { create_payment }.to raise_error(ActiveRecord::RecordInvalid, error_message)
       end
